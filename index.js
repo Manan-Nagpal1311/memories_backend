606a96d31c17be306d75d927ab6fdda11e9aaf55
Manan Nagpal <manan0044.cse19@chitkara.edu.in>
2022-01-27 17:11:05 +0530

    third commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json({limit : "30mb", extended : true}));
 app.use(bodyParser.urlencoded({limit : "30mb", extended : true}));
 app.use(cors(
     {
-        origin:['http://localhost:3000','https://nifty-wiles-2dd542.netlify.app/'],
+        origin:['http://localhost:3000','https://loving-poincare-56ab69.netlify.app/'],
         credentials:true
     },
     )); 
