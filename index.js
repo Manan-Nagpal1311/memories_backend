8bc7f54e06ab5d5cc422b58a27d4f6e1f6c5e35b
Manan Nagpal <manan0044.cse19@chitkara.edu.in>
2022-01-27 16:36:34 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+// import postRoutes from './routes/posts';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js'
+
+
+'mongodb+srv://MananNagpal:Manan2001@cluster0.it1mu.mongodb.net/memories?retryWrites=true&w=majority';
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json({limit : "30mb", extended : true}));
+app.use(bodyParser.urlencoded({limit : "30mb", extended : true}));
+app.use(cors(
+    {
+        origin:['http://localhost:3000'],
+        credentials:true
+    },
+    )); 
+
+app.use('/posts', postRoutes);
+
+// const CONNECTION_URL=process.env.CONNECTION_URL;
+
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=> {
+        app.listen(PORT,()=> console.log(`Server running on port: ${PORT}`));
+    })
+    .catch((error)=>{
+        console.log(error.message);
+    })
+
+// mongoose.set('useFindAndModify',false);
+
